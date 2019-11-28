97249a13eb67c90ae5dccee0a086907ca0e4e6c4
srbulovicdusan <srbulovicdusan@gmail.com>
2019-11-28 15:41:49 +0100

    init dummy payment service

---
+++ PSE/src/main/java/com/paymentserviceexample/PSE/PseApplication.java
@@ -0,0 +1,13 @@
+package com.paymentserviceexample.PSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseApplication.class, args);
+	}
+
+}
---
+++ PSE/src/test/java/com/paymentserviceexample/PSE/PseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentserviceexample.PSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
